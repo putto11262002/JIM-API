02f301a5224e1dcf4308d0b4d5d719a7e110a3d1
Put Suthisrisinlpa <sutpu703@student.otago.ac.nz>
2024-04-03 22:26:35 +0700

    remove unused import (fix build error)

--- packages/dashboard/src/pages/view-model/index.tsx
+++ packages/dashboard/src/pages/view-model/index.tsx
@@ -1,5 +1,4 @@
 import modelService from "../../services/model";
-import { getAppError } from "../../lib/error";
 import { Button } from "@components/ui/button";
 import { Input } from "../../components/ui/input";
 import { Plus } from "lucide-react";
